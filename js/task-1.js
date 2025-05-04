2a35ce9f56a3c51d74617d455ad0406a81cdf632
Nevlen3 <nevlen3@gmail.com>
2025-05-04 14:19:11 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,29 @@
+function isEnoughCapacity(products, containerSize) {
+  const totalProducts = Object.values(products).reduce((sum, quantity) => sum + quantity, 0);
+  return totalProducts <= containerSize;
+}
+
+// Перевірка
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+
+
+/* 
+Оголошена функція isEnoughCapacity(products, containerSize) 
+Виклик isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8) повертає true
+Виклик isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12) повертає false
+Виклик isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14) повертає true
+Виклик isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7) повертає false */
